60d3d3792cd7cae6800d719d24c8768c84d403b3
josephl37 <josephliow2000@gmail.com>
2023-02-28 16:24:01 -0600

    set up react,redux,bootstrap boilerplate

---
+++ src/components/Search.js
@@ -0,0 +1,11 @@
+const Search = () => {
+
+  return (
+    //TODO: fill out HTML w/ bootstrap styling
+    <div>
+      <h1 className="text-center">Search Component</h1>
+    </div>
+  )
+}
+
+export default Search;
---
+++ src/reducers/reducer-user.js
@@ -0,0 +1,5 @@
+//TODO: import action 
+
+//TODO: set up default state
+
+//TODO: set up userReducer function
