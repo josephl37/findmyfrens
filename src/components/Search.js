27978e49a42381fd3c21a918f46c4c185d0da4cd
ocneb <111916951+bcsurf2822@users.noreply.github.com>
2023-03-01 15:58:45 -0500

    able to search for query

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,6 +1,17 @@
 import logo from "../public/wordart.png";
+import { useState } from "react";
+import { fetchUser } from "../actions";
 
 const Search = () => {
+  const [search, setSearch] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    fetchUser(search)
+  }
+
+
+
   return (
     //TODO: fill out HTML w/ bootstrap styling
     <div>
@@ -8,10 +19,14 @@ const Search = () => {
         <img className="img-fluid mt-5" src={logo} alt="findmyfrens logo" />
       </div>
       <div className="mt-3">
-        <input className="form-control" type="text" placeholder="Search an ETH address or ENS" ></input>
+        <form onSubmit={handleSubmit}>
+        <input className="form-control" value={search} onChange={(e) => setSearch(e.target.value)} type="text" required placeholder="Search an ETH address or ENS" ></input>
+        <button className="btn btn-primary">SearchFrens</button>
+        </form>
       </div>
     </div>
   );
 };
+
 
 export default Search;
