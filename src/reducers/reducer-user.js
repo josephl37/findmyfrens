8bd642717501c962054bae86965c0f96748b3064
josephl37 <josephliow2000@gmail.com>
2023-03-01 15:11:45 -0600

    added styling to result

--- src/reducers/reducer-user.js
+++ src/reducers/reducer-user.js
@@ -2,4 +2,17 @@
 
 //TODO: set up default state
 
+const DEFAULT_STATE = {
+  address: "0x17a059b6b0c8af433032d554b0392995155452e6",
+  ens: "brenner.eth",
+  twitter: "brennerspear",
+  avatar_url: "https://avatars.githubusercontent.com/u/12127609?v=4"
+}
+
 //TODO: set up userReducer function
+/*
+const userReducer = function (state = DEFAULT_STATE, action) {
+
+}
+*/
+//export default userReducer;
