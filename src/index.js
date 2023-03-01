e0cf3d202a53b0e3cba98ddd28ec08792cd79d13
ocneb <111916951+bcsurf2822@users.noreply.github.com>
2023-03-01 17:14:33 -0500

    added reduxDevTools to store

--- src/index.js
+++ src/index.js
@@ -7,14 +7,17 @@ import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import reducers from "./reducers";
 import promise from "redux-promise"
+import { composeWithDevTools } from "redux-devtools-extension";
+
 
 //TODO: set up applyMiddleware and reduxPromises
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
+const toolStore = createStoreWithMiddleware(reducers, composeWithDevTools())
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Provider store={createStoreWithMiddleware(reducers)}>
+    <Provider store={toolStore}>
       <App />
     </Provider>
   </React.StrictMode>
