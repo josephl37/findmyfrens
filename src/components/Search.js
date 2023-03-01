63dfff85d02330da8101a75b3163a61ceedf5cdd
josephl37 <josephliow2000@gmail.com>
2023-03-01 12:20:52 -0600

    added search styling

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,11 +1,17 @@
-const Search = () => {
+import logo from "../public/wordart.png";
 
+const Search = () => {
   return (
     //TODO: fill out HTML w/ bootstrap styling
     <div>
-      <h1 className="text-center">Search Component</h1>
+      <div>
+        <img className="img-fluid mt-5" src={logo} alt="findmyfrens logo" />
+      </div>
+      <div className="mt-3">
+        <input className="form-control" type="text" placeholder="Search an ETH address or ENS" ></input>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Search;
