de18a32bd85c21861e14ecb059e5e43beedea539
josephl37 <josephliow2000@gmail.com>
2023-03-02 10:04:02 -0600

    fixed result bug

--- src/components/Result.js
+++ src/components/Result.js
@@ -3,24 +3,39 @@ import "./Result.css";
 
 const Result = () => {
   // TODO: use useSelector to get state
-  const user = useSelector(state => state.info);
-  console.log(user);
-  
+  const user = useSelector((state) => state.info);
+
+  const renderResults = () => {
+    if (user) {
+      return (
+        <div className="d-flex justify-content-center mt-5">
+          <div className="mx-4">
+            <img
+              className="rounded image_pfp"
+              src={`${user.avatar}`}
+              alt="pfp"
+            />
+          </div>
+          <div>
+            <h3>{user.ens}</h3>
+            <p>{user.address}</p>
+            <p>
+              You can find {user.ens} on{" "}
+              <a href={`https://twitter.com/${user.twitter}`} target="_blank">
+                Twitter
+              </a>
+            </p>
+          </div>
+        </div>
+      );
+    }
+  };
+
   return (
-    //TODO: fill out HTML w/ bootstrap styling
     <div>
-      <div className="d-flex justify-content-center mt-5">
-        <div className="mx-4">
-          <img className="rounded image_pfp" src={`${user.avatar}`} alt="pfp"/>
-        </div>
-        <div>
-          <h3>{user.ens}</h3>
-          <p>{user.address}</p>
-          <p>You can find {user.ens} on <a href={`https://twitter.com/${user.twitter}`} target="_blank">Twitter</a></p>
-        </div>
-      </div>
+      {renderResults()}
     </div>
   );
-}
+};
 
 export default Result;
